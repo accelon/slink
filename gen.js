e596b66bdbe6dbad1c07d6f4ca7c63f554c4d06b
yapcheahshen <yapcheahshen@gmail.com>
2025-03-27 14:33:24 +0800

    output to off

--- gen.js
+++ gen.js
@@ -17,22 +17,45 @@ if (!ctx) {
 ctx.lst=lst;
 ctx.readZipBuffer=JSZip.loadAsync;
 ctx.idmap=Allnames;
-
+const outdir='out/';
 ctx.onDocStart=(ctx,fn)=>{
     ctx.lawid=ctx.idmap[fn];
     ctx.ruleid='';//條號
 }
 ctx.onDocEnd=(ctx,fn)=>{
-    console.log(fn,ctx.currentoutput?.length)
+   console.log(fn,ctx.currentoutput?.length)
+   writeChanged(outdir+ fn.replace('.docx','.off'),output.join('\n'),true);
+   output.length=0;
 }
+const output=[];
 const unknownsegnames={};
 const parseLawRule=(ctx,ruleid,t)=>{
-    ctx.ruleid=ctx.lawid+'.'+ruleid.replace(/\^a/,'');
-    //立法理由，可以取消。
-    //刪除理由，
+    ctx.ruleid=ctx.lawid+ruleid.replace(/\^a/,'');
     const m=t.match(/（([^\^\{}]+)）/);
-    //if (m) console.log(m[1])
-    return t;
+    let caption='';
+    if (m) caption=m[1];
+    return '^article'+ctx.ruleid+(caption?'('+caption+')':'');
+}
+const parseRevision=(line)=>{
+    const m=line.match(/--(\d{2,3})年(\d{1,2})月(\d{1,2})日修正前條文--/);
+    let out='';
+    if (!m){
+        console.log('wrong line',line)
+    } else {
+        const year=(parseInt(m[1])+1911).toString()
+        const month=parseInt(m[2]).toString();
+        const day=parseInt(m[3]).toString();
+        const m2=line.match(/\{ln:"([^"]+)",id:"([^"]+)"\}\[修正理由\]/);
+        let reason='';
+        if (m2) {
+            reason=',ln:"'+m2[1]+'",id:"'+m2[2]+'"';
+            out='^rev{date:'+year+month.padStart(2,'0')+day.padStart(2,'0')+reason+'}'
+        } else {
+            //console.log('no reason',line)
+        }
+        
+    }
+    return out;
 }
 ctx.onPara=(ctx,para)=>{
     para=tidy(para)
@@ -41,6 +64,7 @@ ctx.onPara=(ctx,para)=>{
     const m=para.match(/bm\{id:"([^\}第]+)"\}第([^條]+)條之?([\d]*)/);
     if (m) { //法條開始
         out=parseLawRule(ctx , m[1], para.slice(m.index+m[0].length));
+        output.push(out)
     } else {
         let lines=para.replace(/【/g,'\n【').split('\n').filter(it=>!!it);
         for (let i=0;i<lines.length;i++) {
@@ -51,18 +75,23 @@ ctx.onPara=(ctx,para)=>{
                 if (Segnames[segname]) {
                     ctx.segid=Segnames[segname];
                 } 
+                
                 if (!unknownsegnames[segname]) unknownsegnames[segname]=0;
                 unknownsegnames[segname]++;
-                
-            } else if (~line.indexOf('修正前條文')){ 
-                //parseRevision(line);
-                
-            }        
+
+                output.push((ctx.segid?'^seg#'+ctx.segid+' ':'^t('+ segname+')')+line.slice(segname.length+2));
+            } else {
+                if (~line.indexOf('修正前條文')){ 
+                    output.push(parseRevision(line));
+                } else {
+                    output.push(line)
+                }
+            }
         }
     }
     return out;
 }
 await processDocuments(ctx);
-writeChanged('unknownsegnames.txt',fromObj(unknownsegnames,true).join('\n'),true)
+//writeChanged('unknownsegnames.txt',fromObj(unknownsegnames,true).join('\n'),true)
 //fs.wconsole.log( )
-console.log(fromObj(unknownsegnames,true))
+//console.log(fromObj(unknownsegnames,true))
